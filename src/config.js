a8b040baa47a898a45415a91c78549ea8135e2a0
LINDBURG <monc2170@gmail.com>
2019-10-13 05:34:49 +0900

    Setting up out React app:

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "note-app-lindburg"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://buovth6tne.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_IJt6Xr0kf",
+    APP_CLIENT_ID: "4dqceqlkpvbjrkn36obes7uiv6",
+    IDENTITY_POOL_ID: "us-east-2:67adb485-a9c5-4e29-a7fd-e4e35725ab6a"
+  }
+};
